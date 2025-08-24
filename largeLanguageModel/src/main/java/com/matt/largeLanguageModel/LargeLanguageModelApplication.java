91a1473f2f1ffa558a81090c10974257180ef2d5
MattYu12138 <164812398+MattYu12138@users.noreply.github.com>
2025-08-25 08:12:03 +1000

    init

---
+++ largeLanguageModel/src/main/java/com/matt/largeLanguageModel/LargeLanguageModelApplication.java
@@ -0,0 +1,13 @@
+package com.matt.largeLanguageModel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeLanguageModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeLanguageModelApplication.class, args);
+	}
+
+}
---
+++ largeLanguageModel/src/test/java/com/matt/largeLanguageModel/LargeLanguageModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matt.largeLanguageModel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeLanguageModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
